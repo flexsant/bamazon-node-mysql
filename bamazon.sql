e5864871c7b1df69b18d6c5b7b251dc755d9a0cc
flexsant <flexsant@gmail.com>
2019-10-02 14:52:23 -0700

    bamazon.js connection to local host

--- bamazon.sql
+++ bamazon.sql
@@ -13,33 +13,33 @@ CREATE TABLE products (
 );
 
 INSERT INTO products (id, product_name, department_name, price, stock_quantity)
-VALUES (1, "Sex Wax", "Surf Accesories", "2.99", 100);
+VALUES (0001, "Sex Wax", "Surf Accesories", "2.99", 100);
 
 INSERT INTO products (id, product_name, department_name, price, stock_quantity)
-VALUES (3, "Sticky-Bumps", "Surf Accesories", "2.99", 100);
+VALUES (0002, "Sticky-Bumps", "Surf Accesories", "2.99", 100);
 
 INSERT INTO products (id, product_name, department_name, price, stock_quantity)
-VALUES (5, "Ball-16oz-Mason-Jar-4pk", "Kitchen_Home", "9.39", 30);
+VALUES (0003, "Ball-16oz-Mason-Jar-4pk", "Kitchen_Home", "9.39", 30);
 
 INSERT INTO products (id, product_name, department_name, price, stock_quantity)
-VALUES (7, "Heath-Multi-Stem_Vase", "Kitchen-Home", "102.00", 10);
+VALUES (0004, "Heath-Multi-Stem_Vase", "Kitchen-Home", "102.00", 10);
 
 INSERT INTO products (id, product_name, department_name, price, stock_quantity)
-VALUES (17, "Cactus-Paddle", "Kitchen-Home", "20.00", 5);
+VALUES (0005, "Cactus-Paddle", "Kitchen-Home", "20.00", 5);
 
 INSERT INTO products (id, product_name, department_name, price, stock_quantity)
-VALUES (8, "Trucker-Hat-Blk", "Mens-Clothing", "15.00", 35);
+VALUES (0006, "Trucker-Hat-Blk", "Mens-Clothing", "15.00", 35);
 
 INSERT INTO products (id, product_name, department_name, price, stock_quantity)
-VALUES (9, "Organic-Plain-Tee-Blk", "Mens-Clothing", "19.99", 50);
+VALUES (0007, "Organic-Plain-Tee-Blk", "Mens-Clothing", "19.99", 50);
 
 INSERT INTO products (id, product_name, department_name, price, stock_quantity)
-VALUES (11, "Organic-Bamboo-Boxers-Blk", "Mens-Clothing", "16.99", 25);
+VALUES (0008, "Organic-Bamboo-Boxers-Blk", "Mens-Clothing", "16.99", 25);
 
 INSERT INTO products (id, product_name, department_name, price, stock_quantity)
-VALUES (13, "Organic-Canvas-Bag-Tan", "Kitchen-Home", "9.99", 50);
+VALUES (0009, "Organic-Canvas-Bag-Tan", "Kitchen-Home", "9.99", 50);
 
 INSERT INTO products (id, product_name, department_name, price, stock_quantity)
-VALUES (15, "Recycled-Plastic-Bag", "Kitchen-Home", "9.99", 100);
+VALUES (0010, "Recycled-Plastic-Bag", "Kitchen-Home", "9.99", 100);
 
 
