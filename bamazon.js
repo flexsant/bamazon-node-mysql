35945887785f4eb7b2a2b73b44b9d243d41f1968
flexsant <flexsant@gmail.com>
2019-10-02 15:56:12 -0700

    functions inventoryDisplay start

--- bamazon.js
+++ bamazon.js
@@ -16,8 +16,37 @@ var connection = mysql.createConnection({
   database: "bamazon_DB"
 });
 
-connection.connect(function(err) {
-    if (err) throw err;
-    // run the start function after the connection is made to prompt the user
-    start();
-  });
+connection.connect(function (err) {
+  if (err) throw err;
+  // run the start function after the connection is made to prompt the user
+  inventoryDisplay();
+});
+
+function inventoryDisplay() {
+    connection.query("SELECT * FROM products", function(err, res) {
+      if (err) throw err;
+      for (var i = 0; i < res.length; i ++) {
+      console.log(res[i].id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity);
+      }
+      start();
+    })
+
+}
+function start() {
+  inquirer
+    .prompt([
+      {
+        name: "product",
+        type: "number",
+        message: "What is the ID of the product you would like to buy?",
+      },
+      {
+        name: "quantity",
+        type: "number",
+        message: "How many units of the product would you like to buy?",
+      }
+    ]).then(function(answer) {
+
+
+    })
+  }
